0da85484dfb7d733e5d9c5a734bce130578ce8a9
KRSBX <firdaus.sati51@gmail.com>
2022-05-19 22:05:46 +0700

    chore: init react native

---
+++ babel.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          components: './src/components',
+          utils: './src/utils',
+          screens: './src/screens',
+          store: './src/store',
+        },
+      },
+    ],
+  ],
+};
